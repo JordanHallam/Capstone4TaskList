27ccf538d3fce9935ad5b16ca52b859b1c14d504
JordanHallam <jghals12@gmail.com>
2020-08-30 20:02:46 -0400

    Initial Commit

---
+++ Capstone4TaskList/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Capstone4TaskList.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone4TaskList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly aspnetCapstone4TaskListContext _context;
+        public HomeController(aspnetCapstone4TaskListContext context)
+        {
+            _context = context;
+        }
+        
+       [HttpGet]
+       public IActionResult AddThingToDo()
+        {
+            return View();
+        }
+        
+        
+        [HttpPost]
+        public IActionResult AddThingToDo(ThingsToDo thing)
+        {
+            if (ModelState.IsValid)
+            {
+                string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                thing.UserId = id;
+                _context.ThingsToDo.Add(thing);
+                _context.SaveChanges();
+
+            }
+
+            return RedirectToAction("ViewThingsToDo");
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        [Authorize]
+        public IActionResult ViewThingsToDo()
+        {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+           
+            var taskList = _context.ThingsToDo.Where(x => x.UserId == id).ToList();
+            return View(taskList);
+        }
+
+        public IActionResult DeleteThingToDo(int id)
+        {
+            var deleteThing = _context.ThingsToDo.Find(id);
+            if (deleteThing != null)
+            {
+                
+                _context.ThingsToDo.Remove(deleteThing);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("ViewThingsToDo");
+        }
+        public IActionResult ChangeCompletion(int idIndex)
+        {
+            var thing = _context.ThingsToDo.Find(idIndex);
+            if (thing != null)
+            {
+                if (thing.Completion == true)
+                {
+                    thing.Completion = false;
+                }
+                else
+                {
+                    thing.Completion = true;
+                }
+
+            _context.Entry(thing).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.Update(thing);
+            _context.SaveChanges();
+            }
+           return RedirectToAction("ViewThingsToDo");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Capstone4TaskList/Models/ThingsToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone4TaskList.Models
+{
+    public partial class ThingsToDo
+    {
+        public int Id { get; set; }
+        public string ThingDescription { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool? Completion { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
